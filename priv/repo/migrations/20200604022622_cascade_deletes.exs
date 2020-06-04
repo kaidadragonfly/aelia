73512718a09e6ef7423ee70d5ce1f6c2d874606f
Kaida <kaidadragonfly@gmail.com>
2020-06-03 19:31:44 -0700

    Cascade deletes

---
+++ priv/repo/migrations/20200604022622_cascade_deletes.exs
@@ -0,0 +1,17 @@
+defmodule Aelia.Repo.Migrations.CascadeDeletes do
+  use Ecto.Migration
+
+  def change do
+    alter table("folders") do
+      modify :artist_id,
+        references("artists", type: :string, on_delete: :delete_all),
+        from: references("artists")
+    end
+
+    alter table("works") do
+      modify :folder_id,
+        references("folders", type: :string, on_delete: :delete_all),
+        from: references("folders")
+    end
+  end
+end
