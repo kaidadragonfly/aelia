5a70d6c4971f3474f71321ada44475138eaf284e
Kaida <kaidadragonfly@gmail.com>
2020-08-02 12:28:45 -0700

    Clean up configuration

--- config/config.exs
+++ config/config.exs
@@ -28,8 +28,6 @@ config :phoenix, :json_library, Jason
 
 config :aelia, :da_base_url, "https://www.deviantart.com/api/v1/oauth2"
 config :aelia, :da_auth_url, "https://www.deviantart.com/oauth2/token"
-config :aelia, :da_client_id, System.get_env("CLIENT_ID")
-config :aelia, :da_client_secret, System.get_env("CLIENT_SECRET")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
